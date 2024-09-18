e780ac6b712b8fe9f38013b48ae0947a331fcd90
deeplyfriedchicken <kevin.a.cunanan@gmail.com>
2024-09-18 14:49:02 -0700

    added 9man nationals

--- routes/index.tsx
+++ routes/index.tsx
@@ -5,6 +5,7 @@ const Links = [
   { name: 'Instagram', imgPath: "/instagram.webp", url: "https://www.instagram.com/jumping.is.good/" },
   { name: 'Oakland Mini 6/2/2024', imgPath: "/google_drive.webp", url: "https://drive.google.com/drive/folders/1UFbx9db0--c-cyZmcXnqut3eu2D_x-5v?usp=drive_link" },
   { name: 'LA Mini 2024', imgPath: "/google_drive.webp", url: "https://drive.google.com/drive/folders/1LOoQaPULizTWSIHuTPT58T_T9Q0BlDgw?usp=drive_link" },
+  { name: 'SF 9MAN Nationals 2024', imgPath: "/google_drive.webp", url: "https://drive.google.com/drive/folders/1ew5iuq4YMUdqpcnN--7--nS3w-doBCL6?usp=sharing" },
 ]
 
 export default function Home() {
