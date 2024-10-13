c57acdc59467a5f723de959027e6d7ba415faec9
Kevin Cunanan <kevin.a.cunanan@gmail.com>
2024-10-13 00:17:06 -0700

    Added volleyboo link

--- routes/index.tsx
+++ routes/index.tsx
@@ -6,6 +6,7 @@ const Links = [
   { name: 'Oakland Mini 6/2/2024', imgPath: "/google_drive.webp", url: "https://drive.google.com/drive/folders/1UFbx9db0--c-cyZmcXnqut3eu2D_x-5v?usp=drive_link" },
   { name: 'LA Mini 2024', imgPath: "/google_drive.webp", url: "https://drive.google.com/drive/folders/1LOoQaPULizTWSIHuTPT58T_T9Q0BlDgw?usp=drive_link" },
   { name: 'SF 9MAN Nationals 2024', imgPath: "/google_drive.webp", url: "https://drive.google.com/drive/folders/1ew5iuq4YMUdqpcnN--7--nS3w-doBCL6?usp=sharing" },
+  { name: 'Volleyboo 2024', imgPath: "/google_drive.webp", url: "https://drive.google.com/drive/folders/1hJfMv3wl37WMBvDo4u4Bp-SDzOqqKniQ" },
 ]
 
 export default function Home() {
